dab635af5c10a313ed7d044029e8632a66bff626
Luca Arrotta <luca.arrotta@studenti.unimi.it>
2021-04-02 16:08:07 +0200

    first commit

---
+++ src/main/java/actor/Server.java
@@ -0,0 +1,24 @@
+package actor;
+
+import it.ewlab.actor.ActorOuterClass.Actor;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    public static void main(String[] args) throws IOException {
+
+
+        ServerSocket serverSocket = new ServerSocket(9999);
+
+        Socket s = serverSocket.accept();
+
+        Actor actor = Actor.parseFrom(s.getInputStream());
+
+        System.out.println(actor);
+    }
+
+
+}
